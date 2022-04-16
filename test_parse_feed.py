fe1cfa668ef00e8a461816d8ad0fe77a13c4f7f6
Luke <luke@lukewiebe.com>
2022-04-16 18:25:26 -0500

    added date checking to test_parse_feed.py

--- test_parse_feed.py
+++ test_parse_feed.py
@@ -1,9 +1,22 @@
-# Parse a given feed for new entries.
+# Find new entries in a given feed.
 # Collect title, publish time, source for each entry.
 
-from xml.etree import ElementTree as ET
+import feedparser
+import time
 
-with open('samplefeed.xml', 'r') as f:
-	tree = ET.parse(f)
+# Replace samplefeed.xml with url to use in real life
+d = feedparser.parse(r'samplefeed.xml')
 
-print(tree.findall('/'))
+published_time = d.entries[0].published_parsed
+current_time = time.localtime() # should this be .gmtime()?
+
+# convert times to days since epoch
+published_time = time.mktime(published_time) / 86400
+current_time = time.mktime(current_time) / 86400
+
+elapsed_time = current_time - published_time
+
+if elapsed_time > 1:
+	print("Success! The difference between times is more than 1 day.")
+else:
+	print("Something went wrong here.")
