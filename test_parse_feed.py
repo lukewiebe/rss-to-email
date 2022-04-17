8e80aa21e59be2ac41d86bdf13a51c93d83f3d03
Luke <luke@lukewiebe.com>
2022-04-16 19:35:01 -0500

    Cleaned up date math by removing conversion from seconds to days

--- test_parse_feed.py
+++ test_parse_feed.py
@@ -11,12 +11,12 @@ published_time = d.entries[0].published_parsed
 current_time = time.localtime() # should this be .gmtime()?
 
 # convert times to days since epoch
-published_time = time.mktime(published_time) / 86400
-current_time = time.mktime(current_time) / 86400
+published_time = time.mktime(published_time)
+current_time = time.mktime(current_time)
 
 elapsed_time = current_time - published_time
 
-if elapsed_time > 1:
+if elapsed_time > 86400: # 86400 is number of seconds in a day
 	print("Success! The difference between times is more than 1 day.")
 else:
 	print("Something went wrong here.")
