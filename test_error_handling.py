2b8191ae34496699eefc3a4f64fd06619dffd4b8
Luke <mail@lukewiebe.com>
2022-05-30 14:20:25 -0500

    slight reorder of test_error_handling.py

--- test_error_handling.py
+++ test_error_handling.py
@@ -7,14 +7,15 @@ import logging
 import time
 
 # Trying with no feed as input.
-feed_source = ''
+feed_source = 'https://ia.net/feed'
 d = feedparser.parse(feed_source)
 
+if 'title' in d.feed:
+	print(f"title: {'title' in d.feed}")
+else:
+	logging.error('Title does not exist in feed')
+
 try:
-	if 'title' in d.feed:
-		print(f"title: {'title' in d.feed}")
-	else:
-		logging.error('Title does not exist in feed')
 	print(f"first entry: {d.entries[0]}")
 except IndexError:
 	logging.error('IndexError: there are no entries in this feed')
